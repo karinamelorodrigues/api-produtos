8edf17c65aa0b8a598b5af913eab88068a48f2b0
Karina Melo Rodrigues <karina.rodrigues@nb-will0576.avista.local>
2022-03-02 00:20:15 -0300

    feat: integrating with mysql and add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Livro } from './livro.model';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
+      database: process.env.NOME_BANCO_DADOS,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
   controllers: [AppController, LivrosController],
   providers: [AppService, LivrosService],
 })
